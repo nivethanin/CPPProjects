f045e7d83b190838fe62bc7965c0b16a94cb2e98
Nivethan <nivethanin@gmail.com>
2025-01-22 00:46:23 -0500

    Learning absolute basics

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int file_size = 3;
+    double sales = 9.99;
+    int swap = 45;
+    swap = file_size;
+    cout << swap 
+         << " is the number I'm streaming" 
+         << endl;
+    cout << "Hello World" << endl;
+    return 0;
+}
