41c6ec836eea9f83bc66d21c1f26fb744ac75865
Nivethan <nivethanin@gmail.com>
2025-01-22 00:56:44 -0500

    Start of hex to base64 question

---
+++ convert_hex_to_base_64.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+vector<string> splitUpHexValues(const string& str) {
+    /*Splits up hex string into a vector 
+    for hex values */
+    vector<string> result;
+    for (size_t i = 0; i < str.length(); i += 2) {
+        result.push_back(str.substr(i, 2));
+    }
+    return result;
+}
+
+int main()
+{
+    string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    vector<string> parts = splitUpHexValues(input);
+
+    for (const auto& part : parts) {
+        cout << part << endl;
+    }
+
+    return 0;
+}
